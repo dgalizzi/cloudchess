cf847ca819b89614ef8517f0fb8d3804dffc22a5
Diego D. Galizzi <dgalizzi@gmail.com>
2020-02-07 21:36:44 -0300

    Research on uvloop requirement
    Sanic tries to use uvloop if available, here's the relevant part from
    the sanic's source code:
    
    try:
        import uvloop  # type: ignore
    
        if not isinstance(asyncio.get_event_loop_policy(), uvloop.EventLoopPolicy):
            asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
    except ImportError:
        pass
    
    But python-chess engine's asyncio mode doesn't support
    uvloop, so uvloop needs to be unavailable so sanic doesn't use it.

--- main.py
+++ main.py
@@ -41,9 +41,11 @@ async def connect(request, ws):
     # TODO: Taken from an example from the documentation.
     # Seems to work fine without this though.
     asyncio.set_event_loop_policy(chess.engine.EventLoopPolicy())
+
     # TODO: Close the engine at some point?
     # TODO: Close the transport?
     transport, engine = await chess.engine.popen_uci("stockfish")
+
     # TODO: Configure through the API
     await engine.configure({"Threads": 4, "Hash": 1024})
     infiniteAnalysisTask = None
@@ -72,8 +74,5 @@ async def connect(request, ws):
                 infiniteAnalysisTask = loop.create_task(infiniteAnalysis(engine, board, ws))
 
 if __name__ == '__main__':
-    # TODO: Had problems running the engine together with Sanic.
-    # It only started working after I uninstalled uvloop.
-    # Learn about what's going on.
     app.run(host="0.0.0.0", port=5000, protocol=WebSocketProtocol)
 
